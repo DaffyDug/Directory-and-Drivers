3b8a37c8c55b49534035293a7e8a6d2b38127213
Александр Александр <stripalol@inbox.ru>
2024-03-25 16:52:22 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp45/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DriveInfo[] driveInfos = DriveInfo.GetDrives();
+        foreach (var item in driveInfos)
+        {
+            if (item.IsReady)
+            {
+                Console.WriteLine(item.RootDirectory);
+            }
+            if (Directory.Exists($@"{item.RootDirectory}\Windows"))
+            {
+                Console.WriteLine("такая папака есть");
+            }
+            else
+            {
+                Console.WriteLine("такой папки нет");
+            }
+        }
+    }
+}
+public static class TOGBjhj
+{
+    public static long ToTb(this long b)
+    {
+        return b / 1000000000000;
+    }
+}
+
+public static class DraverHelper
+{
+    public static void Avalible()
+    {
+        int procent;
+        DriveInfo[] driveInfos = DriveInfo.GetDrives();
+        foreach (var item in driveInfos)
+        {
+            if (item.IsReady)
+            {
+                procent = (int)((float)item.TotalFreeSpace / (float)item.TotalSize * 100);
+                Console.WriteLine(procent);
+            }
+        }
+    }
+}
+
+
